6033c53d545ab0b2f305bac972d894bff41bc233
Tomer Baum <tomer.baum@observeit.com>
2019-10-11 16:13:31 +0300

    fix

--- Video4/try1.js
+++ Video4/try1.js
@@ -3,3 +3,22 @@ let y = 20;
 let result = ( x * y);
 console.log(result);
 
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("file:///var/lib/jenkins/config.xml");
